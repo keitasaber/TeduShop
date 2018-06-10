0526813bd007e118801613822e821251b1110fc2
Your Name <keitasaber@gmail.com>
2018-06-10 22:35:41 +0700

    add entity framework code first for data

---
+++ TeduShop.Data/Infrastructure/Disposable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Data.Infrastructure
+{
+    class Disposable : IDisposable // tu dong huy
+    {
+        private bool isDisposed;
+
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!isDisposed && disposing)
+            {
+                DisposeCore();
+            }
+            isDisposed = true;
+        }
+
+        protected virtual void DisposeCore()
+        {
+        }
+    }
+}
